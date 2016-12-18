6a491f9ce7a6db09baabc2fcb152541188c6d805
Carlos Iriarte <ciriarte@gmail.com>
2016-12-18 11:02:02 -0800

    feat(add): color triangle

---
+++ rust/color_triangle/src/main.rs
@@ -0,0 +1,84 @@
+#[macro_use]
+extern crate glium;
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
+
+fn main() {
+    use glium::{ DisplayBuild, Surface };
+    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25] };
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+        out vec2 my_attr;
+
+        uniform mat4 matrix;
+
+        void main() {
+            my_attr = position;
+            gl_Position = matrix * vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+        in vec2 my_attr;
+        out vec4 color;
+
+        void main() {
+            color = vec4(my_attr, 0.0, 1.0);
+        }
+    "#;    
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+    let mut t: f32 = -0.5;
+
+    loop {
+        // we update `t`
+        t += 0.0002;
+        if t > 0.5 {
+            t = -0.5;
+        }
+
+        let uniforms = uniform! {
+            matrix: [
+                [ t.cos(), t.sin(), 0.0, 0.0],
+                [-t.sin(), t.cos(), 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0f32],
+            ]
+        };
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+        target.draw(&vertex_buffer, &indices, &program, &uniforms,
+                    &Default::default()).unwrap();
+
+        target.finish().unwrap();
+
+        // listing the events produced by the window and waiting to be received
+        for ev in display.poll_events() {
+            match ev {
+                glium::glutin::Event::Closed => return,   // the window has been closed by the user
+                _ => ()
+            }
+        }
+    }
+}
